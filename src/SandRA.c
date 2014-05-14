ab12b8f94c5baba901ed73dd08ec038897c15925
cejote <thieme@mpimp-golm.mpg.de>
2014-05-14 20:56:21 +0200

    added blast function call; read-in user suppled adapter list

--- src/SandRA.c
+++ src/SandRA.c
@@ -39,37 +39,15 @@
 
 #define MAXCOUNT 5
 
-#define READER1  50000
-#define READER2 100000
-#define READER3	400000
-#define READER4 800000
-#define WRITER1  150000
+#define MAXADAPTERNUM 500
+
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
 
-static int data = 1;
-
 
 // structs
-
-typedef struct {
-	pthread_mutex_t *mut;
-	int writers;
-	int readers;
-	int waiting;
-	pthread_cond_t *writeOK, *readOK;
-} rwl;
-
-typedef struct {
-	rwl *lock;
-	int id;
-	long delay;
-} rwargs;
-
-
-
 
 typedef struct pe_data
 {
@@ -108,18 +86,6 @@ typedef struct se_data
 
 
 // prototypes
-
-rwl *initlock(void);
-void readlock(rwl *lock, int d);
-void writelock(rwl *lock, int d);
-void readunlock(rwl *lock);
-void writeunlock(rwl *lock);
-void deletelock(rwl *lock);
-rwargs *init_thread_data (rwl *l, int i, long d);
-void *read_processing_thread (void *args);
-void *read_providing_thread (void *args);
-
-
 
 
 
@@ -788,10 +754,150 @@ void trim_read(char * read, char * phred, int minqual, int qualtype)
 
 
 
+char** read_adapters_from_file(FILE *fp)
+{
+	/*
+	 * read in list of user-given adapter sequences
+	 *
+	 * lines with ">" are ignored
+	 *
+	 */
+
+	char** adapters = (char**)malloc(sizeof(char*) * MAXADAPTERNUM);
+    char tmpline[MAXREADLEN];
+
+    int cnt=0;
+
+	while(1)
+	{
+		if (NULL == fgets(tmpline, MAXREADLEN-1, fp))
+		{
+			break;
+		}
+
+		if ('>' == tmpline[0])
+		{
+			//assume fasta header - skip line
+			remove_newline(tmpline);
+			//printf("SKIPPING > %s\n", tmpline);
+		}
+		else
+		{
+			remove_newline(tmpline);
+			if (valid_characters(tmpline))
+			{
+				fprintf(stderr,"Found adapter %s\n", tmpline);
+				char *data1=(char*)calloc(MAXREADLEN,sizeof(char));
+				strcpy(data1, tmpline);
+				adapters[cnt]=data1;
+				++cnt;
+			}
+			else
+			{
+				fprintf(stderr,"Invalid adapter %s\n", tmpline);
+			}
+		}
+	}
+
+	return adapters;
+}
+
+
+
+int blast_adapters(char** adapters)
+{
+	//write data to file
+	FILE *fp;
+	int i;
+
+	fp = fopen("blastlist.fasta", "w+");
+	if (fp == NULL)
+	{
+		printf("I couldn't open blastlist.fasta for writing.\n");
+		return -1;
+	}
+
+	i=0;
+	while(NULL!=adapters[i])
+	{
+		fprintf(fp, ">adapter_%d\n%s\n", i, adapters[i]);
+		++i;
+	}
+	fclose(fp);
+
+
+	//makeblastdb -in blastlistref.fasta -dbtype nucl -out blastlistref.db
+	//blastn -db blastref/blastlistref.db -num_threads 10 -outfmt "7 sseqid ssac qstart qend sstart send qseq evalue bitscore" -query blastlist.fasta -perc_identity 80
+
+
+
+	//blast file
+	FILE *blastpipe;
+	blastpipe = popen("blastn -db blastref/blastlistref.db -num_threads 10 -outfmt 6 -query blastlist.fasta -perc_identity 80", "r");
+	if (blastpipe != NULL)
+	{
+		while (1)
+		{
+			char *line;
+			char buf[1000];
+			line = fgets(buf, sizeof buf, blastpipe);
+			if (line == NULL) break;
+
+			//todo parse results
+			//todo filter adapter
+			printf("%s", line);
+		}
+		pclose(blastpipe);
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
 int main(int argc, const char** argv)
 {
 
 
+	//when file with adapter sequences is supplied...
+    char * adptfle="/home/thieme/eclipse-workspace/SandRA/adapters.fasta";
+
+    if (!file_exists(adptfle)) {
+        printf("File failed to open: %s\n", adptfle);
+        exit(EXIT_FAILURE);
+      }
+
+
+    FILE *adptflep = fopen(adptfle, "r");
+
+    char** adapters2;
+    adapters2=read_adapters_from_file(adptflep);
+
+	int i=0;
+	while(NULL!=adapters2[i])
+	{
+		printf("%d: %s\n", i, adapters2[i]);
+		++i;
+	}
+
+
+
+	//test for the blast procedure for auto-detected adapters
+	blast_adapters(adapters2);
+
+	return 0;
+
+
+
+
+	//trim @N test case
 	char * c = "GTGCTAGCTGATGCTGCGTAGCTGCATCTG";
 	char * p = "123456789012345678901234567890";
 
@@ -900,55 +1006,17 @@ int main(int argc, const char** argv)
 
 
 
+    //todo: read_adapters_from_file()
+
+
+
+
+
 
     srand(time(NULL));
 
 
 
-    if (0)
-    {
-
-	pthread_t r1, r2, r3, r4, w1;
-	rwargs *a1, *a2, *a3, *a4, *a5;
-	rwl *lock;
-
-	lock = initlock();
-	a1 = init_thread_data (lock, 1, WRITER1);
-	pthread_create (&w1, NULL, read_providing_thread, a1);
-
-
-
-	a2 = init_thread_data (lock, 1, READER1);
-	a3 = init_thread_data (lock, 2, READER2);
-	a4 = init_thread_data (lock, 3, READER3);
-	a5 = init_thread_data (lock, 4, READER4);
-
-	pthread_create (&r1, NULL, read_processing_thread, a2);
-	pthread_create (&r2, NULL, read_processing_thread, a3);
-	pthread_create (&r3, NULL, read_processing_thread, a4);
-	pthread_create (&r4, NULL, read_processing_thread, a5);
-
-	pthread_join (w1, NULL);
-	pthread_join (r1, NULL);
-	pthread_join (r2, NULL);
-	pthread_join (r3, NULL);
-	pthread_join (r4, NULL);
-
-
-	//finalize
-	free(a1);
-	free(a2);
-	free(a3);
-	free(a4);
-	free(a5);
-
-
-
-
-
-
-	return 0;
-	}
 
 
 
@@ -1093,10 +1161,10 @@ int main(int argc, const char** argv)
 
         if (n_splitting>0)
         {
-        	longest_valid_section(readlist[readentrycnt]->read1, readlist[readentrycnt]->phred1);
+        	trim_to_longest_valid_section(readlist[readentrycnt]->read1, readlist[readentrycnt]->phred1);
 			if (PE)
 			{
-	        	longest_valid_section(readlist[readentrycnt]->read2, readlist[readentrycnt]->phred2);
+				trim_to_longest_valid_section(readlist[readentrycnt]->read2, readlist[readentrycnt]->phred2);
 			}
         }
 
@@ -1174,192 +1242,3 @@ int main(int argc, const char** argv)
 
 
 
-
-/*
- * begin thread based stuff
- */
-
-
-
-rwargs *init_thread_data (rwl *l, int i, long d)
-{
-	/*
-	 *
-	 * init object for each thread
-	 *
-	 */
-
-
-	rwargs *args;
-	args = (rwargs *)malloc (sizeof (rwargs));
-	if (args == NULL) return (NULL);
-	args->lock = l;
-	args->id = i;
-	args->delay = d;
-	return (args);
-}
-
-void *read_processing_thread (void *args)
-{
-	/*
-	 *
-	 * consumer
-	 *
-	 *
-	 */
-
-
-	rwargs *a;
-	int d;
-
-	a = (rwargs *)args;
-
-	do {
-		readlock (a->lock, a->id);
-		d = data;
-		usleep (a->delay);
-		readunlock (a->lock);
-		printf ("Trimmer %d : Data = %d\n", a->id, d);
-		usleep (a->delay);
-	} while (d != 0);
-	printf ("Trimmer %d: Finished.\n", a->id);
-
-	return (NULL);
-}
-
-void *read_providing_thread (void *args)
-{
-	/*
-	 *
-	 * producer
-	 *
-	 *
-	 */
-
-
-	rwargs *a;
-	int i;
-
-	a = (rwargs *)args;
-
-	for (i = 2; i < MAXCOUNT; i++) {
-		writelock (a->lock, a->id);
-		data = i;
-		usleep (a->delay);
-		writeunlock (a->lock);
-		printf ("read_providing_thread %d: Wrote %d\n", a->id, i);
-		usleep (a->delay);
-	}
-	printf ("read_providing_thread %d: Finishing...\n", a->id);
-	writelock (a->lock, a->id);
-	data = 0;
-	writeunlock (a->lock);
-	printf ("read_providing_thread %d: Finished.\n", a->id);
-
-	return (NULL);
-}
-
-
-
-
-
-/*
- * thread locking
- */
-
-rwl *initlock (void)
-{
-	/*
-	 *
-	 * malloc and init with 0
-	 *
-	 *
-	 */
-
-
-	rwl *lock;
-	lock = (rwl *)malloc (sizeof (rwl));
-	if (lock == NULL) return (NULL);
-
-	lock->mut = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
-	if (lock->mut == NULL) { free (lock); return (NULL); }
-
-	lock->writeOK =		(pthread_cond_t *) malloc (sizeof (pthread_cond_t));
-	if (lock->writeOK == NULL) { free (lock->mut); free (lock); return (NULL); }
-
-	lock->readOK =		(pthread_cond_t *) malloc (sizeof (pthread_cond_t));
-	if (lock->writeOK == NULL) { free (lock->mut); free (lock->writeOK); free (lock); return (NULL); }
-
-	pthread_mutex_init (lock->mut, NULL);
-	pthread_cond_init (lock->writeOK, NULL);
-	pthread_cond_init (lock->readOK, NULL);
-	lock->readers = 0;
-	lock->writers = 0;
-	lock->waiting = 0;
-
-	return (lock);
-}
-
-void readlock (rwl *lock, int d)
-{
-	pthread_mutex_lock (lock->mut);
-	if (lock->writers || lock->waiting)
-	{
-		do {
-			printf ("reader %d blocked.\n", d);
-			pthread_cond_wait (lock->readOK, lock->mut);
-			printf ("reader %d unblocked.\n", d);
-		} while (lock->writers);
-	}
-	lock->readers++;
-	pthread_mutex_unlock (lock->mut);
-
-	return;
-}
-
-void readunlock (rwl *lock)
-{
-	pthread_mutex_lock (lock->mut);
-	lock->readers--;
-	pthread_cond_signal (lock->writeOK);
-	pthread_mutex_unlock (lock->mut);
-}
-
-
-void writelock (rwl *lock, int d)
-{
-	pthread_mutex_lock (lock->mut);
-	lock->waiting++;
-	while (lock->readers || lock->writers) {
-		printf ("read_providing_thread %d blocked.\n", d);
-		pthread_cond_wait (lock->writeOK, lock->mut);
-		printf ("read_providing_thread %d unblocked.\n", d);
-	}
-	lock->waiting--;
-	lock->writers++;
-	pthread_mutex_unlock (lock->mut);
-
-	return;
-}
-
-void writeunlock (rwl *lock)
-{
-	pthread_mutex_lock (lock->mut);
-	lock->writers--;
-	pthread_cond_broadcast (lock->readOK);
-	pthread_mutex_unlock (lock->mut);
-}
-
-void deletelock (rwl *lock)
-{
-	pthread_mutex_destroy (lock->mut);
-	pthread_cond_destroy (lock->readOK);
-	pthread_cond_destroy (lock->writeOK);
-	free (lock);
-
-	return;
-}
-
-/*
- * end thread based stuff
- */
